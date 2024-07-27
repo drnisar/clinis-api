56a88da8d3148592ada4758aedb0e5883e6841d1
drnisar <drnisar@hotmail.com>
2024-07-27 12:20:27 +0500

    reverted to 13 days back

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-});
 app.use(cors());
 
 // Routes
